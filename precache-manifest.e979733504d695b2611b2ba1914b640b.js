a36cc377dfad614398d4bbc6ed8fff65e460c8e2
Cristian Nica <cristian.nica@zenitech.co.uk>
2020-02-13 09:42:51 +0200

    Updates

---
+++ precache-manifest.e979733504d695b2611b2ba1914b640b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e3b8bd939e0b4fcdbf9222f995067a",
+    "url": "/reusable-react-components-pg/index.html"
+  },
+  {
+    "revision": "08b82dd2a7efcd473f12",
+    "url": "/reusable-react-components-pg/static/css/2.52738377.chunk.css"
+  },
+  {
+    "revision": "1419fac6d5a948b11ea8",
+    "url": "/reusable-react-components-pg/static/css/main.01444971.chunk.css"
+  },
+  {
+    "revision": "08b82dd2a7efcd473f12",
+    "url": "/reusable-react-components-pg/static/js/2.75793030.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/reusable-react-components-pg/static/js/2.75793030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1419fac6d5a948b11ea8",
+    "url": "/reusable-react-components-pg/static/js/main.56c6a68e.chunk.js"
+  },
+  {
+    "revision": "4a8fee78abfdffa43a95",
+    "url": "/reusable-react-components-pg/static/js/runtime-main.b850546f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.70d56105932e20c56d8bc18655014fc3.js"
+  "/reusable-react-components-pg/precache-manifest.e979733504d695b2611b2ba1914b640b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reusable-react-components-pg/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
