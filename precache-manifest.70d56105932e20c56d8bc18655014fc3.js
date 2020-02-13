68afd697a5d45ed01e0cd812f571e3f05b6914d4
Cristian Nica <cristian.nica@zenitech.co.uk>
2020-02-13 09:39:13 +0200

    Updates

---
+++ precache-manifest.70d56105932e20c56d8bc18655014fc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c61a2661117e54f5b5051d113c198e40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08b82dd2a7efcd473f12",
+    "url": "/static/css/2.52738377.chunk.css"
+  },
+  {
+    "revision": "9e092cb7094f3b41d01e",
+    "url": "/static/css/main.01444971.chunk.css"
+  },
+  {
+    "revision": "08b82dd2a7efcd473f12",
+    "url": "/static/js/2.75793030.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.75793030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e092cb7094f3b41d01e",
+    "url": "/static/js/main.31e2450e.chunk.js"
+  },
+  {
+    "revision": "184d40a7fa4cbc1914b6",
+    "url": "/static/js/runtime-main.49d32fc0.js"
+  }
+]);
